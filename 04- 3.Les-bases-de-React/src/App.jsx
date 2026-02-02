5d604a5d6a62a65d9f94d0c39f35228f6e7d6183
asonkes <audrey.sonkes@gmail.com>
2026-02-02 15:14:52 +0100

    exemple de toggle

--- 04- 3.Les-bases-de-React/src/App.jsx
+++ 04- 3.Les-bases-de-React/src/App.jsx
@@ -1,10 +1,10 @@
-import Container11 from "./lessons/11.Short-circuit/Container";
+import Container12 from "./lessons/12.Toggle-une-classe-ou-du-CSS/Container";
 
 function App() {
   return (
     <div className="main-content">
       <h1>Cours React</h1>
-      <Container11 />
+      <Container12 />
     </div>
   );
 }
--- 04- 3.Les-bases-de-React/src/lessons/12.Toggle-une-classe-ou-du-CSS/Container.jsx
+++ 04- 3.Les-bases-de-React/src/lessons/12.Toggle-une-classe-ou-du-CSS/Container.jsx
@@ -1,9 +1,30 @@
-import "./Container.css"
+import { useState } from "react";
+import "./Container.css";
 
-export default function Container() {
+export default function Container12() {
+  // On va simuler un "darkMode"
+  // Et on met false, car à la base, le mode n'est pas en dark
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
-    <div>
-      <h1>Toggle une classe</h1>
-    </div>
-  )
+    <>
+      {/** Ajoute la classe 'darkMode' uniquement si 'darkMode === true' */}
+      <div className={`fullContainer ${darkMode && "dark-mode"}`}>
+        <h1>Toggle une classe</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+          dignissimos tempore deserunt repellat aliquam veniam quaerat earum
+          sint laborum sed nostrum consectetur dolorum blanditiis voluptatum,
+          exercitationem, commodi illo! Ratione, esse!
+        </p>
+
+        {/** Si darkMode === false devient true */}
+        {/** Si darkMode === true devient false */}
+        {/** Ca ca sert à changer la valeur du darkMode (false ou true) */}
+        <button onClick={() => setDarkMode(!darkMode)}>
+          Activer le {darkMode ? "light Mode" : "Dark mode"}
+        </button>
+      </div>
+    </>
+  );
 }
