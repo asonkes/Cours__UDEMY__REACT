c83d55affe0aebaa6eeec67fdda2897397203b5b
asonkes <audrey.sonkes@gmail.com>
2026-02-02 14:46:47 +0100

    exercice todolist

---
+++ 05- 4.Project To-do list/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import "./App.css";
+import { ListItem } from "./components/ListItem";
+
+function App() {
+  // Donc chaque objet va avoir une propriété content (ex: propriété 'content 1');
+  const [todoList, setTodoList] = useState([
+    // Donc ici, on utilise la méthode 'nanoid' pour donner un 'id' à chaque élément
+    {
+      id: nanoid(8),
+      content: "item 1",
+    },
+
+    {
+      id: nanoid(8),
+      content: "item 2",
+    },
+
+    {
+      id: nanoid(8),
+      content: "item 3",
+    },
+  ]);
+
+  // On a bien un 'id' qui a été créé
+  console.log(todoList);
+
+  const deleteTodo = (id) => {
+    setTodoList(todoList.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div className="h-screen bg-slate-900">
+      <div className="max-w-4xl mx-auto pt-20 px-6">
+        <h1 className="text-3xl text-slate-100 mb-4">La To-do liste</h1>
+
+        <form className="mb-10">
+          <label htmlFor="todo-item" className="text-slate-50">
+            Ajouter une chose à faire
+          </label>
+          <input type="text" className="mt-1 block w-full rounded" />
+          <button className="mt-4 mb-4 py-2 px-2 bg-slate-50 rounded min-w-[115px]">
+            Ajouter
+          </button>
+          <ul>
+            {todoList.map((item) => (
+              <ListItem key={item.id} itemData={item} deleteTodo={deleteTodo} />
+            ))}
+          </ul>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
